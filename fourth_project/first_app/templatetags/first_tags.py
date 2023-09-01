8aa5cc438f2d7bbbb27a9f236ad3bdc4b455c30f
Md .  Shah Alam <94603993+MdShah-alam@users.noreply.github.com>
2023-09-01 11:32:24 +0600

    Add files via upload

---
+++ fourth_project/first_app/templatetags/first_tags.py
@@ -0,0 +1,36 @@
+from django import template
+from django.template.loader import get_template
+
+
+register = template.Library()
+
+def my_template(value , arg ):
+    if arg == 'change':
+        value = "Rahim"
+        return value
+    if arg == 'title':
+        return value.title()
+    
+def show_courses():
+    courses = [
+        {
+            "id" : 1,
+            "course" : "C",
+            "teacher" : "Rahm"
+        },
+        {
+            "id" : 2,
+            "course" : "C++",
+            "teacher" : "karim"
+        },
+        {
+            "id" : 3,
+            "course" : "Python",
+            "teacher" : "Roni"
+        }
+    ]
+    return {'courses' : courses}
+courses_template = get_template('first_app/courses.html')
+    
+register.filter('change_name',my_template)
+register.inclusion_tag(courses_template)(show_courses)
